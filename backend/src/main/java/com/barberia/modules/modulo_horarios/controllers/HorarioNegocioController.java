84eca135b905484ba19e78d7dddc9b79dc3bc43b
jmc929 <josemiguel.correa@udea.edu.co>
2026-04-30 19:25:56 -0500

    versionamiento apis

--- backend/src/main/java/com/barberia/modules/modulo_horarios/controllers/HorarioNegocioController.java
+++ backend/src/main/java/com/barberia/modules/modulo_horarios/controllers/HorarioNegocioController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/horarios")
+@RequestMapping("/api/v1/horarios")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class HorarioNegocioController {
 
--- backend/src/main/java/com/barberia/modules/modulo_servicios/controllers/ServicioController.java
+++ backend/src/main/java/com/barberia/modules/modulo_servicios/controllers/ServicioController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/servicios")
+@RequestMapping("/api/v1/servicios")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ServicioController {
 
